ce139f426847f5fada0efac9ccb7df935abfc6ec
DESKTOP-1T4GM7V\test <hioapp@gethio.com>
2018-12-18 14:14:25 +0330

    -init

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("HIOChrome")]
+[assembly: AssemblyDescription("HIO Technologies Inc.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HIO Technologies Inc.")]
+[assembly: AssemblyProduct("HIOChrome")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0ce8b4dd-1a40-42f3-9a4e-07b00e0c8b22")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.49.0")]
+[assembly: AssemblyFileVersion("1.0.49.0")]
